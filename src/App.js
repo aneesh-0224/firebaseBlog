b1c5d06b6e45f245d53797377201990093726765
unknown <aneesh0224@gmail.com>
2023-01-15 16:32:13 +0530

    basic structure done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {Routes,Route} from 'react-router-dom'
+import Home from "./Components/Home";
+import Login from './Components/Login';
+import CreatePost from './Components/CreatePost';
+import React from 'react';
+import Navbar from './Components/Navbar';
+import { useState } from 'react';
+const authContext=React.createContext()
 function App() {
+  let isAuthLocal=localStorage.getItem('isAuth')
+  console.log(isAuthLocal)
+  if(!isAuthLocal||isAuthLocal=='false'){
+    isAuthLocal=false
+  }
+  const [isAuth,setIsAuth]=useState(isAuthLocal)
+
   return (
+    <authContext.Provider value={{isAuth,setIsAuth}}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+        <Routes>
+          <Route path="/" element={ <Home/> } />
+          <Route path="/login" element={ <Login/> } />
+          <Route path="/createPost" element={ <CreatePost/> } />
+      </Routes>
     </div>
+    </authContext.Provider>
   );
 }
 
-export default App;
+export {App,authContext}
